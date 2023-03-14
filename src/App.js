b6291f36fc1054f3745fca9383cdd440498bdad5
ThibaultEspag <thibaulte@sipios.com>
2023-03-14 12:20:38 +0100

    add everything

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Oh Lucie ça va?</p>
+        <iframe src="https://giphy.com/embed/Pxqkke0OO5zUyRo9OF" width="480" height="269" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+        <button>Yes</button>
+        <button>No</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
